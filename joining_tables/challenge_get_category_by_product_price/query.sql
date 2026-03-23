c0b588509ac358928e2371bb6459e79fbdc88dfe
shruthiamar <>
2026-03-23 23:41:57 +0000

    Challenge: Get Category By Product Price

--- joining_tables/challenge_get_category_by_product_price/query.sql
+++ joining_tables/challenge_get_category_by_product_price/query.sql
@@ -1,1 +1,4 @@
-SELECT DISTINCT category.name JOIN product ON category.id = product.category_id WHERE price > 450;
+SELECT DISTINCT category.name FROM category
+    JOIN product 
+    ON category.id = product.category_id 
+    WHERE price > 450;
